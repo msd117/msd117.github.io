4ae66a743538c2850809a68e99a724127346abbe
swxuser <msd117@yahoo.com>
2018-04-30 17:04:52 +0530

    Content Modified

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to         = 'trendytheme.net@gmail.com';  // put your email here
+    $to         = 'deshmukh.sohel@yahoo.com';  // put your email here
 
     $email_template = 'simple.html';
 
