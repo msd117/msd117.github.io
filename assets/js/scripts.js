638c4dbf1078442ba84e2b29dde896c4c9e4bf6e
swxuser <msd117@yahoo.com>
2018-04-30 17:27:56 +0530

    bug fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(19.907723,75.3417737);
 
             var styles = [
                 {
